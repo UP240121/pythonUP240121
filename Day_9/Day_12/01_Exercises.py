3089e7eb22fac3717a8ade93714fa5a718c348a1
UP240121 <up240121@alumnos.upa.edu.mx>
2025-04-06 17:10:37 -0600

    Add files via upload

---
+++ Day_9/Day_12/01_Exercises.py
@@ -0,0 +1,26 @@
+#1, 2, 3
+import random
+def random_user_id():
+    s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    res = ''
+    for i in range(6):
+        res += random.choice(s)
+    return res
+
+
+def user_id_gen_by_user():
+    n = int(input("Enter n : "))
+    t = int(input("Enter t : "))
+    s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    for i in range(t):
+        res = ''
+        for j in range(n):
+            res += random.choice(s)
+        print(res)
+
+
+def rgb_color_gen():
+    r = random.randint(0, 256)
+    b = random.randint(0, 256)
+    g = random.randint(0, 256)
+    return f"rgb({r},{b},{g})"
